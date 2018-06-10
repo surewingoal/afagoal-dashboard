35dbb679f0bce5b2d4bb5040f2664603f7ca9c07
xiaxi <xiaxi@jianbaolife.com>
2018-06-10 08:39:37 +0800

    用户注册

--- src/main/java/com/afagoal/security/SecurityContext.java
+++ src/main/java/com/afagoal/security/SecurityContext.java
@@ -10,21 +10,30 @@ import org.springframework.security.core.context.SecurityContextHolder;
  */
 public class SecurityContext {
 
-    public static SysUser currentUser(){
-        AfagoalUser user = (AfagoalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(null == user){
-           return emptyUser();
+    private final static String ANONYMOUS_USER = "anonymousUser";
+
+    public static SysUser currentUser() {
+        Object userObject = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(userObject);
+        AfagoalUser user;
+        if (userObject instanceof AfagoalUser) {
+            user = (AfagoalUser) userObject;
+        } else if (userObject instanceof String && ANONYMOUS_USER.equals(userObject)) {
+            return emptyUser();
+        } else {
+            throw new RuntimeException("unknown type of user!");
         }
+
         SysUser sysUser = new SysUser();
         sysUser.setId(user.getId());
         sysUser.setUserName(user.getUsername());
         return sysUser;
     }
 
-    private static SysUser emptyUser(){
+    private static SysUser emptyUser() {
         SysUser sysUser = new SysUser();
         sysUser.setId(Integer.valueOf(-1));
-        sysUser.setUserName("未知用户");
+        sysUser.setUserName(ANONYMOUS_USER);
         return sysUser;
     }
 
