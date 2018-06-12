78fe34e9e7953d65943b3ebb8cb657cc2ed1e46c
xiaxi <xiaxi@jianbaolife.com>
2018-06-12 17:38:24 +0800

    rename

--- src/main/java/com/afagoal/config/MailConfig.java
+++ src/main/java/com/afagoal/config/MailConfig.java
@@ -1,15 +1,13 @@
 package com.afagoal.config;
 
-import com.afagoal.mail.AfagoalMainProperties;
+import com.afagoal.mail.AfagoalMailProperties;
 import com.afagoal.mail.AfagoalMainSender;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 /**
  * Created by BaoCai on 18/6/11.
@@ -22,7 +20,7 @@ public class MailConfig {
     private JavaMailSender javaMailSender;
 
     @Bean
-    public AfagoalMainSender afagoalMainSender(AfagoalMainProperties afagoalMainProperties) {
+    public AfagoalMainSender afagoalMainSender(AfagoalMailProperties afagoalMainProperties) {
         AfagoalMainSender afagoalMainSender = new AfagoalMainSender(javaMailSender);
         afagoalMainSender.setOrganization(afagoalMainProperties.getOrganization());
         afagoalMainSender.setBaseContent(afagoalMainProperties.getBaseContent());
@@ -32,8 +30,8 @@ public class MailConfig {
 
     @Bean(name = "afagoalMainProperties")
     @ConfigurationProperties(prefix = "afagoal.mail")
-    public AfagoalMainProperties afagoalMainProperties() {
-        return new AfagoalMainProperties();
+    public AfagoalMailProperties afagoalMainProperties() {
+        return new AfagoalMailProperties();
     }
 
 }
